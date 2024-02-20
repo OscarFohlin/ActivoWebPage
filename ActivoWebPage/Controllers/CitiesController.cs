cc1a11bf994ca270bb6f3be8493633650db3cceb
EmmiieJ <158288919+EmmiieJ@users.noreply.github.com>
2024-02-20 13:25:35 +0100

    Update CitiesController.cs

--- ActivoWebPage/Controllers/CitiesController.cs
+++ ActivoWebPage/Controllers/CitiesController.cs
@@ -1,1 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActivoWebPage.Controllers
+{
+    public class CitiesController : Controller
+    {
+        public IActionResult Trollhattan()
+        {
+            return View();
+        }
+        public IActionResult Vastervik()
+        {
+            return View();
+        }
+    }
+}
 
