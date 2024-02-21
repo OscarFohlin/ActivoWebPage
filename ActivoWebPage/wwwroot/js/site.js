c247e108d9bb2a317ea8301eaa96d8f817a8ba21
OscarFohlin <56166104+OscarFohlin@users.noreply.github.com>
2024-02-21 15:01:11 +0100

    Update site.js - script for button in hero header
    skrollar användaren längst ner på hemsidan. iaf just nu går at äåndra så att den kan ghöra vad viu vill senare

--- ActivoWebPage/wwwroot/js/site.js
+++ ActivoWebPage/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function scrollToBottom() {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+}
