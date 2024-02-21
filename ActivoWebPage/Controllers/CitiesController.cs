212a0b99726554a05a1d643f7475009b88630543
FLinnell <158289006+FLinnell@users.noreply.github.com>
2024-02-21 15:16:40 +0100

    Update CitiesController.cs
    Uppdaterad för att kunna visa nya flikar

--- ActivoWebPage/Controllers/CitiesController.cs
+++ ActivoWebPage/Controllers/CitiesController.cs
@@ -4,11 +4,27 @@ namespace ActivoWebPage.Controllers
 {
     public class CitiesController : Controller
     {
+        //Startsidor
         public IActionResult Trollhattan()
         {
             return View();
         }
+
         public IActionResult Vastervik()
+        {
+            return View();
+        }
+
+        //Flikar för Trollhättan
+        public IActionResult ThnCulture()
+        {
+            return View();
+        }
+        public IActionResult ThnSports()
+        {
+            return View();
+        }
+        public IActionResult ThnSearch()
         {
             return View();
         }
