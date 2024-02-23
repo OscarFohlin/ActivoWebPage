ed9bfd27ef07013d1eb6d3a35e62116c91a4494a
OscarFohlin <56166104+OscarFohlin@users.noreply.github.com>
2024-02-23 23:36:22 +0100

    QoL i header
    la till javascript funktionalitet i header men en del URLs behöver ändras inför javaskriptet

--- ActivoWebPage/wwwroot/js/site.js
+++ ActivoWebPage/wwwroot/js/site.js
@@ -6,3 +6,27 @@
 function scrollToBottom() {
     window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
 }
+
+//kolla URL
+var currentUrl = window.location.href;
+
+//kolla om URL stämmer
+if (currentUrl.includes("Trollhattan")) {
+    //fixa stilar/funktioner beroende på det som ska hända om URL:en stämmer
+    var navLink = document.querySelector('a.nav-link.dropdown-toggle');
+
+    navLink.textContent = 'Trollhättan';
+    navLink.classList.add('chosen-section-highlight-text');
+}
+
+//kolla om URL stämmer
+if (currentUrl.includes("Vastervik")) {
+    //fixa stilar/funktioner beroende på det som ska hända om URL:en stämmer
+    var navLink = document.querySelector('a.nav-link.dropdown-toggle');
+
+    navLink.textContent = 'Västervik';
+    navLink.classList.add('chosen-section-highlight-text');
+}
+
+    //ändra URL:s så att de som tillhär västervik har 'vastervik' i URL för att 
+    //javascript ska funka på alla sidor utan att behöva görta en massa onödig kod
