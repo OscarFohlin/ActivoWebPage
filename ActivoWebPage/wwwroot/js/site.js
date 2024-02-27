064abe9e4df16b992d25ec548deb3e80e5fe9811
OscarFohlin <56166104+OscarFohlin@users.noreply.github.com>
2024-02-27 12:00:56 +0100

    Update site.js

--- ActivoWebPage/wwwroot/js/site.js
+++ ActivoWebPage/wwwroot/js/site.js
@@ -3,6 +3,7 @@
 
 // Write your JavaScript code.
 
+//hero header pil ner script temp funktionalitet
 function scrollToBottom() {
     window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
 }
@@ -14,14 +15,15 @@ var currentUrl = window.location.href;
 if (currentUrl.includes("Trollhattan")) {
     //fixa stilar/funktioner beroende på det som ska hända om URL:en stämmer
     var navLink = document.querySelector('a.nav-link.dropdown-toggle');
-
+    //sätt stad namn som användaren valt
     navLink.textContent = 'Trollhättan';
+    //ändra font stil
     navLink.classList.add('chosen-section-highlight-text');
 }
 
-//kolla om URL stämmer
+
 if (currentUrl.includes("Vastervik")) {
-    //fixa stilar/funktioner beroende på det som ska hända om URL:en stämmer
+
     var navLink = document.querySelector('a.nav-link.dropdown-toggle');
 
     navLink.textContent = 'Västervik';
@@ -33,7 +35,6 @@ if (currentUrl.includes("Trollhattan") & currentUrl.includes("Sports")) {
 
     var navLink = document.querySelector('#trollhattan-sports');
 
-    navLink.textContent = 'SPORT & FRILUFTSLIV';
     navLink.classList.add('chosen-section-highlight-text');
 
 } 
@@ -42,7 +43,6 @@ if (currentUrl.includes("Trollhattan") & currentUrl.includes("Socializing")) {
 
     var navLink = document.querySelector('#trollhattan-socializing');
 
-    navLink.textContent = 'ÄTA & DRICKA';
     navLink.classList.add('chosen-section-highlight-text');
 
 }
@@ -51,7 +51,6 @@ if (currentUrl.includes("Trollhattan") & currentUrl.includes("Culture")) {
 
     var navLink = document.querySelector('#trollhattan-culture');
 
-    navLink.textContent = 'KULTUR & NÖJE';
     navLink.classList.add('chosen-section-highlight-text');
 
 }
@@ -60,16 +59,5 @@ if (currentUrl.includes("Trollhattan") & currentUrl.includes("Search")) {
 
     var navLink = document.querySelector('body > header > nav > div.nav-item.dropdown > a:nth-child(2)');
 
-    navLink.textContent = 'SÖK EVENT';
     navLink.classList.add('chosen-section-highlight-text');
 }
-
-
-//smooth transistion bland hemsidan, tas förmodligen bort idk
-window.addEventListener('load', function () {
-    var loadingScreen = document.querySelector('.loading-screen');
-    loadingScreen.classList.add('hidden');
-});
-
-    //ändra URL:s så att de som tillhär västervik har 'vastervik' i URL för att 
-    //javascript ska funka på alla sidor utan att behöva görta en massa onödig kod
