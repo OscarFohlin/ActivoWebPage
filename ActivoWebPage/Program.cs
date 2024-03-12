bbf70a0893e3cda08e8fcb9c57e2a3e2ddadced6
FLinnell <158289006+FLinnell@users.noreply.github.com>
2024-03-12 16:07:31 +0100

    Update Program.cs
    Uppdaterad för att hantera sessioner (SSO)

--- ActivoWebPage/Program.cs
+++ ActivoWebPage/Program.cs
@@ -2,8 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//SSO
+builder.Services.AddScoped<AuthenticationService>();
+builder.Services.AddSession();
 
 var app = builder.Build();
+
+app.UseSession();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
