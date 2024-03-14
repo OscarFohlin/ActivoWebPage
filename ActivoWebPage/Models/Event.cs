29ef60dc9d406c61868f0669148490ec1940f760
EmmiieJ <158288919+EmmiieJ@users.noreply.github.com>
2024-03-14 08:46:31 +0100

    Create Event.cs

---
+++ ActivoWebPage/Models/Event.cs
@@ -0,0 +1,31 @@
+namespace ActivoWebPage.Models
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+
+        public int UserID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDateTime { get; set; }
+
+        public DateTime EndDateTime { get; set; }
+
+        public int PlaceID { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string URL { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public string[] ImageData { get; set; }
+
+        public int[] TagID { get; set; }
+    }
+}
