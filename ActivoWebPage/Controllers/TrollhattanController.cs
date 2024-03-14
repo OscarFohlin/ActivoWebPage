230ca6bb515e439aff3bf1c4f6901e96bd85a41a
EmmiieJ <158288919+EmmiieJ@users.noreply.github.com>
2024-03-14 08:44:56 +0100

    Update TrollhattanController.cs
    Api-koppling

--- ActivoWebPage/Controllers/TrollhattanController.cs
+++ ActivoWebPage/Controllers/TrollhattanController.cs
@@ -4,10 +4,34 @@ namespace ActivoWebPage.Controllers
 {
     public class TrollhattanController : Controller
     {
-
+        string apiUrl = "https://informatik4.ei.hv.se/EVENTAPI2/api/events";
+        
         //Flikar för Trollhättan
-        public IActionResult Home()
-        {
+        public async Task<IActionResult> Home()
+            {
+                //Kalla på API och skickar till view genom en datatabel
+                DataTable eventDt = new DataTable();
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(apiUrl);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage getData = await client.GetAsync("");
+
+                    //Installera Newtonsoft.Json paketet
+                    if (getData.IsSuccessStatusCode)
+                    {
+                        string results = getData.Content.ReadAsStringAsync().Result;
+                        eventDt = JsonConvert.DeserializeObject<DataTable>(results);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error calling the API");
+                    }
+
+                    ViewData.Model = eventDt;
+                }
             return View("~/Views/Trollhattan/Home.cshtml");
         }
         public IActionResult Culture()
