d8bd3346d00a0f237e5e4853a937cf0d23829b81
FLinnell <158289006+FLinnell@users.noreply.github.com>
2024-03-14 09:47:52 +0100

    Update TrollhattanController.cs
    Uppdaterad för att hålla koll på inloggningsession

--- ActivoWebPage/Controllers/TrollhattanController.cs
+++ ActivoWebPage/Controllers/TrollhattanController.cs
@@ -1,53 +1,39 @@
-
+using Hv.Sos100.SingleSignOn;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using System.Data;
-using System.Net.Http.Headers;
 
 namespace ActivoWebPage.Controllers
 {
     public class TrollhattanController : Controller
     {
-        string eventApiUrl = "https://informatik4.ei.hv.se/EVENTAPI2/api/events";
-        
+
         //Flikar för Trollhättan
-        public async Task<IActionResult> Home()
-            {
-                //Kalla på API och skickar till view genom en datatabel
-                DataTable eventDt = new DataTable();
-                using (var client = new HttpClient())
-                {
-                    client.BaseAddress = new Uri(eventApiUrl);
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        public async Task<IActionResult> HomeAsync()
+        {
+            var authenticationService = new AuthenticationService();
+            var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
+            authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
 
-                    HttpResponseMessage getData = await client.GetAsync("");
-
-                    //Installera Newtonsoft.Json paketet
-                    if (getData.IsSuccessStatusCode)
-                    {
-                        string results = getData.Content.ReadAsStringAsync().Result;
-                        eventDt = JsonConvert.DeserializeObject<DataTable>(results);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Error calling the API");
-                    }
-
-                    ViewData.Model = eventDt;
-                }
             return View("~/Views/Trollhattan/Home.cshtml");
         }
-        public IActionResult Culture()
+        public async Task<IActionResult> CultureAsync()
         {
+            var authenticationService = new AuthenticationService();
+            var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
+            authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
             return View("~/Views/Trollhattan/Trollhattan_Culture.cshtml");
         }
-        public IActionResult Sports()
+        public async Task<IActionResult> SportsAsync()
         {
+            var authenticationService = new AuthenticationService();
+            var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
+            authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
             return View("~/Views/Trollhattan/Trollhattan_Sports.cshtml");
         }
-        public IActionResult Socializing()
+        public async Task<IActionResult> SocializingAsync()
         {
+            var authenticationService = new AuthenticationService();
+            var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
+            authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
             return View("~/Views/Trollhattan/Trollhattan_Socializing.cshtml");
         }
     }
