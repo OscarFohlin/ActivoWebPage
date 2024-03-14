154aead547a6b20d49d95d9a0a24ecabce0c0be6
FLinnell <158289006+FLinnell@users.noreply.github.com>
2024-03-14 09:47:12 +0100

    Update HomeController.cs
    Uppdaterad för att kunna logga in och hålla sessioner

--- ActivoWebPage/Controllers/HomeController.cs
+++ ActivoWebPage/Controllers/HomeController.cs
@@ -2,6 +2,8 @@ using ActivoWebPage.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Hv.Sos100.SingleSignOn;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
 
 
 namespace ActivoWebPage.Controllers
@@ -18,12 +20,11 @@ namespace ActivoWebPage.Controllers
         //Uppdaterad för att hantera session
         public async Task<IActionResult> Index()
         {
-            // Create an instance of the authenticationservice, be careful not to mix it up with the native .NET AuthenticationService
+            //Läs in om session finns och skicka till ViewData, allt gör SSO NuGet
             var authenticationService = new AuthenticationService();
-
-            // Use the authenticationService object to call the ResumeSession method and optionally store the result in a variable
-            // The ResumeSession method does not need to be supplied with any account information as it checks the browser cookies for a valid token
             var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
+
+            authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
 
             return View();
         }
@@ -33,32 +34,22 @@ namespace ActivoWebPage.Controllers
         {
             return View();
         }
-
         [HttpPost]
         public async Task<IActionResult> Authenticate(string email, string password)
         {
-            // Create an instance of the authenticationservice, be careful not to mix it up with the native .NET AuthenticationService
             var authenticationService = new AuthenticationService();
-
-            // Use the authenticationService object to call the CreateSession method and optionally store the result in a variable
-            // Make sure to supply the CreateSession method with the email, password and controllerBase:this, HttpContext
             var authenticatedSession = await authenticationService.CreateSession(email, password, controllerBase: this, HttpContext);
-
-            // Depending on if the authentication was successfull the account can be directed to different pages
             return authenticatedSession ?  RedirectToAction("Index", "Home") : RedirectToAction("Privacy", "Home");
-          
         }
+
 
         public IActionResult Logout()
         {
-            // Create an instance of the authenticationservice, be careful not to mix it up with the native .NET AuthenticationService
             var authenticationService = new AuthenticationService();
-
-            // Call the EndSession method to delete the cookie and end the account´s session
-            // This will force the account to authenticate themselves again if they want to have access
             authenticationService.EndSession(controllerBase: this, HttpContext);
             return RedirectToAction("Index", "Home");
         }
+
         public IActionResult Privacy()
         {
             return View();
