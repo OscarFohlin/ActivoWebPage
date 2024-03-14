e3d579ac4d1eb18f6a2c61bdcf7a77bc4f9eaea5
FLinnell <158289006+FLinnell@users.noreply.github.com>
2024-03-14 12:39:04 +0100

    Update TrollhattanController.cs
    EventAPI är tillbaka

--- ActivoWebPage/Controllers/TrollhattanController.cs
+++ ActivoWebPage/Controllers/TrollhattanController.cs
@@ -1,20 +1,49 @@
 using Hv.Sos100.SingleSignOn;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
+using System.Net.Http.Headers;
 
 namespace ActivoWebPage.Controllers
 {
     public class TrollhattanController : Controller
     {
+        string eventApiUrl = "https://informatik4.ei.hv.se/EVENTAPI2/api/events";
 
         //Flikar för Trollhättan
-        public async Task<IActionResult> HomeAsync()
+        public async Task<IActionResult> Home()
         {
+            //Kalla på API och skickar till view genom en datatabel
+            DataTable eventDt = new DataTable();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(eventApiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage getData = await client.GetAsync("");
+
+                //Installera Newtonsoft.Json paketet
+                if (getData.IsSuccessStatusCode)
+                {
+                    string results = getData.Content.ReadAsStringAsync().Result;
+                    eventDt = JsonConvert.DeserializeObject<DataTable>(results);
+                }
+                else
+                {
+                    Console.WriteLine("Error calling the API");
+                }
+
+                ViewData.Model = eventDt;
+            }
+
             var authenticationService = new AuthenticationService();
             var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
             authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
 
             return View("~/Views/Trollhattan/Home.cshtml");
         }
+
         public async Task<IActionResult> CultureAsync()
         {
             var authenticationService = new AuthenticationService();
