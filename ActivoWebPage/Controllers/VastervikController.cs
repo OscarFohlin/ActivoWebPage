12a8767a46881b47a338add962d5f8ccf16628ee
EmmiieJ <158288919+EmmiieJ@users.noreply.github.com>
2024-03-14 13:40:26 +0100

    Update VastervikController.cs

--- ActivoWebPage/Controllers/VastervikController.cs
+++ ActivoWebPage/Controllers/VastervikController.cs
@@ -5,34 +5,54 @@ namespace ActivoWebPage.Controllers
 {
     public class VastervikController : Controller
     {
+        private readonly EventApiService _eventApiService;
+
+        public VastervikController(EventApiService eventApiService)
+        {
+            _eventApiService = eventApiService;
+        }
+        
         //Flikar för Västervik
         public async Task<IActionResult> HomeAsync()
         {
+            //Hämtar API-datatable 
+            DataTable eventDt = await _eventApiService.GetEventDataAsync();
+        
             var authenticationService = new AuthenticationService();
             var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
             authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
-            return View("~/Views/Vastervik/Home.cshtml");
+            return View("~/Views/Vastervik/Home.cshtml", eventDt);
         }
+        
         public async Task<IActionResult> CultureAsync()
         {
+            //Hämtar API-datatable 
+            DataTable eventDt = await _eventApiService.GetEventDataAsync();
+            
             var authenticationService = new AuthenticationService();
             var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
             authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
-            return View("~/Views/Vastervik/Vastervik_Culture.cshtml");
+            return View("~/Views/Vastervik/Vastervik_Culture.cshtml", eventDt);
         }
         public async Task<IActionResult> SportsAsync()
         {
+            //Hämtar API-datatable 
+            DataTable eventDt = await _eventApiService.GetEventDataAsync();
+            
             var authenticationService = new AuthenticationService();
             var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
             authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
-            return View("~/Views/Vastervik/Vastervik_Sports.cshtml");
+            return View("~/Views/Vastervik/Vastervik_Sports.cshtml", eventDt);
         }
         public async Task<IActionResult> SocializingAsync()
         {
+            //Hämtar API-datatable 
+            DataTable eventDt = await _eventApiService.GetEventDataAsync();
+            
             var authenticationService = new AuthenticationService();
             var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
             authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
-            return View("~/Views/Vastervik/Vastervik_Socializing.cshtml");
+            return View("~/Views/Vastervik/Vastervik_Socializing.cshtml", eventDt);
         }
     }
 }
