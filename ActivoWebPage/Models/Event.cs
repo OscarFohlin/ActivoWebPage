f09e64828a35655473d2f46a993d8079b95afb73
FLinnell <158289006+FLinnell@users.noreply.github.com>
2024-03-14 14:56:24 +0100

    Update Event.cs
    allt utom ID är nullable

--- ActivoWebPage/Models/Event.cs
+++ ActivoWebPage/Models/Event.cs
@@ -6,26 +6,26 @@ namespace ActivoWebPage.Models
 
         public int UserID { get; set; }
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        public DateTime StartDateTime { get; set; }
+        public DateTime? StartDateTime { get; set; }
 
-        public DateTime EndDateTime { get; set; }
+        public DateTime? EndDateTime { get; set; }
 
-        public int PlaceID { get; set; }
+        public int? PlacesID { get; set; }
 
-        public decimal Price { get; set; }
+        public decimal? Price { get; set; }
 
-        public string URL { get; set; }
+        public string? URL { get; set; }
 
-        public int CategoryID { get; set; }
+        public int? CategoryID { get; set; }
 
-        public DateTime TimeStamp { get; set; }
+        public DateTime? TimeStamp { get; set; }
 
-        public string[] ImageData { get; set; }
+        public string[]? ImageData { get; set; }
 
-        public int[] TagID { get; set; }
+        public int[]? TagID { get; set; }
     }
 }
