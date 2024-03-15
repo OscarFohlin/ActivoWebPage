d72e3251160a162a1295ef9ea41b9ece2083edf5
elki0010 <158289011+elki0010@users.noreply.github.com>
2024-03-15 15:28:58 +0100

    Update Activities.cs

--- ActivoWebPage/Models/Activities.cs
+++ ActivoWebPage/Models/Activities.cs
@@ -1,14 +1,28 @@
 using System;
 namespace ActivoWebPage.Models
 {
-	public class Activities
-	{
-		public int activityID { get; set; }
+    public class Activities
+    {
+        public int activityID { get; set; }
 
         public string? name { get; set; }
 
         public string? description { get; set; }
 
         public string? imageLink { get; set; }
+
+        public string? openHours { get; set; }
+
+        public string? url { get; set; }
+
+        public int? categoryID { get; set; }
+
+        public Category[]? category { get; set; }
+
+        public int? placesID { get; set; }
+
+        public DateTime? timestamp { get; set; }
+
+        public Tag[]? tags { get; set; }
     }
 }
