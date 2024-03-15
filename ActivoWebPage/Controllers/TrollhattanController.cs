9bd4c59eca8d6b79488021fa25e54becba99ce6e
Ruba <159074169+Rual0002@users.noreply.github.com>
2024-03-15 10:04:21 +0100

    Update TrollhattanController.cs

--- ActivoWebPage/Controllers/TrollhattanController.cs
+++ ActivoWebPage/Controllers/TrollhattanController.cs
@@ -1,9 +1,8 @@
+using ActivoWebPage.Models;
 using Hv.Sos100.SingleSignOn;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using System.Data;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using static ActivoWebPage.Controllers.HomeController;
 
 namespace ActivoWebPage.Controllers
@@ -16,14 +15,14 @@ namespace ActivoWebPage.Controllers
         {
             _eventApiService = eventApiService;
         }
+
         //Flikar för Trollhättan
         public async Task<IActionResult> Home()
         {
-        
-        //Hämtar data från API och skickar de till view
+            //Hämtar data från API och skickar de till view
             DataTable eventDt = await _eventApiService.GetEventDataAsync();
-            
-            
+
+
             var authenticationService = new AuthenticationService();
             var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
             authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
@@ -35,21 +34,20 @@ namespace ActivoWebPage.Controllers
         {
             //Hämtar data från API och skickar de till view
             DataTable eventDt = await _eventApiService.GetEventDataAsync();
-            
-        
+
             var authenticationService = new AuthenticationService();
             var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
             authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
             return View("~/Views/Trollhattan/Trollhattan_Culture.cshtml", eventDt);
         }
 
-        
+
         public async Task<IActionResult> SportsAsync()
         {
-        //Hämtar data från API och skickar de till view
-         DataTable eventDt = await _eventApiService.GetEventDataAsync();
-         
-         
+            //Hämtar data från API och skickar de till view
+            DataTable eventDt = await _eventApiService.GetEventDataAsync();
+
+
             var authenticationService = new AuthenticationService();
             var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
             authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
@@ -58,22 +56,44 @@ namespace ActivoWebPage.Controllers
         public async Task<IActionResult> SocializingAsync()
         {
             DataTable eventDt = await _eventApiService.GetEventDataAsync();
-        
+
             var authenticationService = new AuthenticationService();
             var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
             authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
             return View("~/Views/Trollhattan/Trollhattan_Socializing.cshtml", eventDt);
         }
-        
+
         public async Task<IActionResult> Search()
         {
-            var authenticationService = new AuthenticationService();
-            var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
-            authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
-
             DataTable eventDt = await _eventApiService.GetEventDataAsync();
 
             return View(eventDt);
+        }
+        public async Task<IActionResult> EventDetails(int eventId)
+        {
+            var eventModel = await _eventApiService.GetEventByIdAsync(eventId);
+            if (eventModel == null)
+            {
+                return View("EventNotFound");
+            }
+           
+            /*var placeModel = await _eventApiService.GetPlaceByIdAsync(eventModel.PlacesID);
+            if (placeModel == null)
+            {
+                return View("PlaceNotFound"); 
+            }*/
+
+            var viewModel = new EventDetailsViewModel
+            {
+                Event = eventModel,
+                //Place = placeModel
+            };
+            return View(viewModel);
+
+        }
+        public IActionResult ActivityDetails()
+        {
+            return View("~/Views/Trollhattan/ActivityDetails.cshtml");
         }
     }
 }
