b36c6ac2a33be102df573576e7c9dc614a18c3ac
elki0010 <158289011+elki0010@users.noreply.github.com>
2024-03-15 12:19:14 +0100

    Create CollectionViewModel.cs

---
+++ ActivoWebPage/Models/CollectionViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Data;
+
+namespace ActivoWebPage.Models
+{
+	public class CollectionViewModel
+	{
+        public DataTable ActivitiesDt { get; set; }
+        public DataTable EventDt { get; set; }
+    }
+}
