2bfdc6e07138998a82a0efc398289260ea0f03eb
elki0010 <158289011+elki0010@users.noreply.github.com>
2024-03-15 12:17:47 +0100

    Update VastervikController.cs

--- ActivoWebPage/Controllers/VastervikController.cs
+++ ActivoWebPage/Controllers/VastervikController.cs
@@ -6,70 +6,115 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using static ActivoWebPage.Controllers.HomeController;
 
+
 namespace ActivoWebPage.Controllers
 {
     public class VastervikController : Controller
     {
-        private readonly EventApiService _eventApiService;
 
-        public VastervikController(EventApiService eventApiService)
+        private readonly EventApiService _eventApiService;
+        private readonly ActivitiesApiService _activitiesApiService;
+
+        public VastervikController(ActivitiesApiService activitiesApiService, EventApiService eventApiService)
         {
             _eventApiService = eventApiService;
+            _activitiesApiService = activitiesApiService;
         }
-        
+
         //Flikar för Västervik
         public async Task<IActionResult> HomeAsync()
         {
-            //Hämtar API-datatable 
+            //Hämtar data från API och skickar de till view
+            DataTable activitiesDt = await _activitiesApiService.GetActivitiesDataAsync();
             DataTable eventDt = await _eventApiService.GetEventDataAsync();
-        
+
+            var viewModel = new CollectionViewModel
+            {
+                ActivitiesDt = activitiesDt,
+                EventDt = eventDt
+            };
+
             var authenticationService = new AuthenticationService();
             var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
             authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
-            return View("~/Views/Vastervik/Home.cshtml", eventDt);
+
+            return View(viewModel);
         }
-        
+
         public async Task<IActionResult> CultureAsync()
         {
-            //Hämtar API-datatable 
+            //Hämtar data från API och skickar de till view
+            DataTable activitiesDt = await _activitiesApiService.GetActivitiesDataAsync();
             DataTable eventDt = await _eventApiService.GetEventDataAsync();
-            
+
+            var viewModel = new CollectionViewModel
+            {
+                ActivitiesDt = activitiesDt,
+                EventDt = eventDt
+            };
+
             var authenticationService = new AuthenticationService();
             var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
             authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
-            return View("~/Views/Vastervik/Vastervik_Culture.cshtml", eventDt);
+
+            return View(viewModel);
         }
         public async Task<IActionResult> SportsAsync()
         {
-            //Hämtar API-datatable 
+            //Hämtar data från API och skickar de till view
+            DataTable activitiesDt = await _activitiesApiService.GetActivitiesDataAsync();
             DataTable eventDt = await _eventApiService.GetEventDataAsync();
-            
+
+            var viewModel = new CollectionViewModel
+            {
+                ActivitiesDt = activitiesDt,
+                EventDt = eventDt
+            };
+
             var authenticationService = new AuthenticationService();
             var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
             authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
-            return View("~/Views/Vastervik/Vastervik_Sports.cshtml", eventDt);
+
+            return View(viewModel);
         }
         public async Task<IActionResult> SocializingAsync()
         {
-            //Hämtar API-datatable 
+            //Hämtar data från API och skickar de till view
+            DataTable activitiesDt = await _activitiesApiService.GetActivitiesDataAsync();
             DataTable eventDt = await _eventApiService.GetEventDataAsync();
-            
+
+            var viewModel = new CollectionViewModel
+            {
+                ActivitiesDt = activitiesDt,
+                EventDt = eventDt
+            };
+
             var authenticationService = new AuthenticationService();
             var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
             authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
-            return View("~/Views/Vastervik/Vastervik_Socializing.cshtml", eventDt);
+
+            return View(viewModel);
         }
 
         public async Task<IActionResult> Search()
         {
+
+            DataTable activitiesDt = await _activitiesApiService.GetActivitiesDataAsync();
+            DataTable eventDt = await _eventApiService.GetEventDataAsync();
+
+            var viewModel = new CollectionViewModel
+            {
+                ActivitiesDt = activitiesDt,
+                EventDt = eventDt
+            };
+
             var authenticationService = new AuthenticationService();
             var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
             authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
 
-            DataTable eventDt = await _eventApiService.GetEventDataAsync();
-
-            return View(eventDt);
+            return View(viewModel);
         }
     }
 }
+
 
