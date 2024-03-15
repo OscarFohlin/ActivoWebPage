92bb143ab2c865404303f9452a4afc895c39cee0
elki0010 <158289011+elki0010@users.noreply.github.com>
2024-03-15 12:18:41 +0100

    Create Activities.cs

---
+++ ActivoWebPage/Models/Activities.cs
@@ -0,0 +1,14 @@
+using System;
+namespace ActivoWebPage.Models
+{
+	public class Activities
+	{
+		public int activityID { get; set; }
+
+        public string? name { get; set; }
+
+        public string? description { get; set; }
+
+        public string? imageLink { get; set; }
+    }
+}
