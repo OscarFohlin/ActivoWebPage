a09a62d0390424621cda48ee228672e61a081fed
Ruba <159074169+Rual0002@users.noreply.github.com>
2024-03-15 10:04:58 +0100

    Update Event.cs

--- ActivoWebPage/Models/Event.cs
+++ ActivoWebPage/Models/Event.cs
@@ -12,7 +12,42 @@ namespace ActivoWebPage.Models
 
         public DateTime? StartDateTime { get; set; }
 
+        public DateTime? EndDateTime { get; set; }namespace ActivoWebPage.Models
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+
+        public int UserID { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime? StartDateTime { get; set; }
+
         public DateTime? EndDateTime { get; set; }
+        
+        public int? AgeLimit { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public string? URL { get; set; }
+
+        public int? CategoryID { get; set; }
+
+        public DateTime? TimeStamp { get; set; }
+
+        public string? ImageLink { get; set; }
+
+        public string? ImageFileName { get; set; }
+
+        public int[]? TagID { get; set; }
+
+        public int PlacesID { get; internal set; }
+    }
+}
+
 
         public int? PlacesID { get; set; }
 
