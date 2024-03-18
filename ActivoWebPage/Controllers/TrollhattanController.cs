1c7cd80008bff395e5431c276ff296f83c7659e8
Ruba <159074169+Rual0002@users.noreply.github.com>
2024-03-18 13:45:49 +0100

    Update TrollhattanController.cs

--- ActivoWebPage/Controllers/TrollhattanController.cs
+++ ActivoWebPage/Controllers/TrollhattanController.cs
@@ -2,56 +2,86 @@ using ActivoWebPage.Models;
 using Hv.Sos100.SingleSignOn;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using static ActivoWebPage.Controllers.HomeController;
+using Activity = ActivoWebPage.Models.Activity;
 
 namespace ActivoWebPage.Controllers
 {
     public class TrollhattanController : Controller
     {
         private readonly EventApiService _eventApiService;
-        private readonly ActivitiesApiService _activitiesApiService;
-        private readonly PlacesApiService _placesApiService;
 
-        public TrollhattanController(ActivitiesApiService activitiesApiService, EventApiService eventApiService, PlacesApiService placesApiService)
+        public TrollhattanController(EventApiService eventApiService)
         {
             _eventApiService = eventApiService;
-            _activitiesApiService = activitiesApiService;
-            _placesApiService = placesApiService;
         }
 
         //Flikar för Trollhättan
         public async Task<IActionResult> Home()
         {
-            //Hämtar data från API och skickar de till view
-            DataTable activitiesDt = await _activitiesApiService.GetActivitiesDataAsync();
-            DataTable eventDt = await _eventApiService.GetEventDataAsync();
-            DataTable placesDt = await _placesApiService.GetPlacesDataAsync();
+            var events = await _eventApiService.GetEventDataAsync();
+            var activities = await _eventApiService.GetActivityDataAsync();
 
-            var viewModel = new CollectionViewModel
+            var filteredActivities = new List<Activity?>();
+            var filteredEvent = new List<Event?>();
+            
+            foreach (var activity in activities)
             {
-                ActivitiesDt = activitiesDt,
-                EventDt = eventDt,
-                PlacesDt = placesDt
-            };
+                var place = await _eventApiService.GetPlaceByIdAsync(activity.PlacesID);
+                if (place?.City?.Equals("Trollhättan", StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    filteredActivities.Add(activity);
+                }
+            }
+            foreach (var ev in events)
+            {
+                var place = await _eventApiService.GetPlaceByIdAsync(ev.PlacesID);
+                if (place?.City?.Equals("Trollhättan", StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    filteredEvent.Add(ev);
+                }
+            }
 
-            var authenticationService = new AuthenticationService();
-            var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
-            authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
+            var viewModel = new HomeViewModel
+            {
+                Events = filteredEvent,
+                Activities = filteredActivities
+            };
 
             return View(viewModel);
         }
 
         public async Task<IActionResult> CultureAsync()
         {
-             //Hämtar data från API och skickar de till view
-            DataTable activitiesDt = await _activitiesApiService.GetActivitiesDataAsync();
-            DataTable eventDt = await _eventApiService.GetEventDataAsync();
+            var events = await _eventApiService.GetEventDataAsync();
+            var activities = await _eventApiService.GetActivityDataAsync();
 
-            var viewModel = new CollectionViewModel
+            var filteredActivities = new List<Activity?>();
+            var filteredEvent = new List<Event?>();
+
+            foreach (var activity in activities)
             {
-                ActivitiesDt = activitiesDt,
-                EventDt = eventDt
+                var place = await _eventApiService.GetPlaceByIdAsync(activity.PlacesID);
+                if (place?.City?.Equals("Trollhättan", StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    filteredActivities.Add(activity);
+                }
+            }
+            foreach (var ev in events)
+            {
+                var place = await _eventApiService.GetPlaceByIdAsync(ev.PlacesID);
+                if (place?.City?.Equals("Trollhättan", StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    filteredEvent.Add(ev);
+                }
+            }
+
+            var viewModel = new HomeViewModel
+            {
+                Events = filteredEvent,
+                Activities = filteredActivities
             };
 
             var authenticationService = new AuthenticationService();
@@ -60,18 +90,36 @@ namespace ActivoWebPage.Controllers
 
             return View(viewModel);
         }
-
 
         public async Task<IActionResult> SportsAsync()
         {
-             //Hämtar data från API och skickar de till view
-            DataTable activitiesDt = await _activitiesApiService.GetActivitiesDataAsync();
-            DataTable eventDt = await _eventApiService.GetEventDataAsync();
+            var events = await _eventApiService.GetEventDataAsync();
+            var activities = await _eventApiService.GetActivityDataAsync();
 
-            var viewModel = new CollectionViewModel
+            var filteredActivities = new List<Activity?>();
+            var filteredEvent = new List<Event?>();
+
+            foreach (var activity in activities)
             {
-                ActivitiesDt = activitiesDt,
-                EventDt = eventDt
+                var place = await _eventApiService.GetPlaceByIdAsync(activity.PlacesID);
+                if (place?.City?.Equals("Trollhättan", StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    filteredActivities.Add(activity);
+                }
+            }
+            foreach (var ev in events)
+            {
+                var place = await _eventApiService.GetPlaceByIdAsync(ev.PlacesID);
+                if (place?.City?.Equals("Trollhättan", StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    filteredEvent.Add(ev);
+                }
+            }
+
+            var viewModel = new HomeViewModel
+            {
+                Events = filteredEvent,
+                Activities = filteredActivities
             };
 
             var authenticationService = new AuthenticationService();
@@ -80,17 +128,36 @@ namespace ActivoWebPage.Controllers
 
             return View(viewModel);
         }
-        
+
         public async Task<IActionResult> SocializingAsync()
         {
-            //Hämtar data från API och skickar de till view
-            DataTable activitiesDt = await _activitiesApiService.GetActivitiesDataAsync();
-            DataTable eventDt = await _eventApiService.GetEventDataAsync();
+            var events = await _eventApiService.GetEventDataAsync();
+            var activities = await _eventApiService.GetActivityDataAsync();
 
-            var viewModel = new CollectionViewModel
+            var filteredActivities = new List<Activity?>();
+            var filteredEvent = new List<Event?>();
+
+            foreach (var activity in activities)
             {
-                ActivitiesDt = activitiesDt,
-                EventDt = eventDt
+                var place = await _eventApiService.GetPlaceByIdAsync(activity.PlacesID);
+                if (place?.City?.Equals("Trollhättan", StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    filteredActivities.Add(activity);
+                }
+            }
+            foreach (var ev in events)
+            {
+                var place = await _eventApiService.GetPlaceByIdAsync(ev.PlacesID);
+                if (place?.City?.Equals("Trollhättan", StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    filteredEvent.Add(ev);
+                }
+            }
+
+            var viewModel = new HomeViewModel
+            {
+                Events = filteredEvent,
+                Activities = filteredActivities
             };
 
             var authenticationService = new AuthenticationService();
@@ -102,61 +169,92 @@ namespace ActivoWebPage.Controllers
 
         public async Task<IActionResult> Search()
         {
-            DataTable activitiesDt = await _activitiesApiService.GetActivitiesDataAsync();
-            DataTable eventDt = await _eventApiService.GetEventDataAsync();
+            var events = await _eventApiService.GetEventDataAsync();
+            var activities = await _eventApiService.GetActivityDataAsync();
 
-            var viewModel = new CollectionViewModel
+            var filteredActivities = new List<Activity?>();
+            var filteredEvent = new List<Event?>();
+
+            foreach (var activity in activities)
             {
-                ActivitiesDt = activitiesDt,
-                EventDt = eventDt
-            };
+                var place = await _eventApiService.GetPlaceByIdAsync(activity.PlacesID);
+                if (place?.City?.Equals("Trollhättan", StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    filteredActivities.Add(activity);
+                }
+            }
+            foreach (var ev in events)
+            {
+                var place = await _eventApiService.GetPlaceByIdAsync(ev.PlacesID);
+                if (place?.City?.Equals("Trollhättan", StringComparison.OrdinalIgnoreCase) == true)
+                {
+                    filteredEvent.Add(ev);
+                }
+            }
 
-            var authenticationService = new AuthenticationService();
-            var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
-            authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
+            var viewModel = new HomeViewModel
+            {
+                Events = filteredEvent,
+                Activities = filteredActivities
+            };
+            return View("Search", viewModel);
+        }
+
+
+        public async Task<IActionResult> EventDetails(int EventID)
+        {
+            var eventModel = await _eventApiService.GetEventByIdAsync(EventID);
+            if (eventModel == null)
+            {
+                return View("EventNotFound");
+            }
+
+            var placeModel = await _eventApiService.GetPlaceByIdAsync(eventModel.PlacesID);
+            if (placeModel == null)
+            {
+                placeModel = new Places
+                {
+                    Name = "Okänd plats",
+                    Address = "Ingen adress tillgänglig"
+                };
+            }
+
+            var viewModel = new EventDetailsViewModel
+            {
+                Event = eventModel,
+                Place = placeModel
+            };
 
             return View(viewModel);
         }
-        
-       public async Task<IActionResult> EventDetails(int eventId)
- {
-     var eventModel = await _eventApiService.GetEventByIdAsync(eventId);
-     if (eventModel == null)
-     {
-         return View("EventNotFound");
-     }
 
-     var placeModel = await _eventApiService.GetPlaceByIdAsync(eventModel.PlacesID);
-     if (placeModel == null)
-     {
-         return View("PlaceNotFound");
-     }
-
-     var viewModel = new EventDetailsViewModel
-     {
-         Event = eventModel,
-         Place = placeModel
-     };
-
-     return View(viewModel);
- }
-
-
- public IActionResult ActivityDetails()
- {
-     return View("~/Views/Trollhattan/ActivityDetails.cshtml");
- }
-        public async Task<IActionResult> ActivityDetailsAsync()
+        public async Task<IActionResult> ActivityDetails(int activityID)
         {
-            DataTable activitiesDt = await _activitiesApiService.GetActivitiesDataAsync();
+            var activityModel = await _eventApiService.GetActivityByIdAsync(activityID);
+            if (activityModel == null)
+            {
+                return View("ActivityNotFound");
+            }
 
+            var placeModel = await _eventApiService.GetPlaceByIdAsync(activityModel.PlacesID);
+            if (placeModel == null)
+            {
+                placeModel = new Places
+                {
+                    Name = "Okänd plats",
+                    Address = "Ingen adress tillgänglig"
+                };
+            }
 
-            var authenticationService = new AuthenticationService();
-            var existingSession = await authenticationService.ResumeSession(controllerBase: this, HttpContext);
-            authenticationService.ReadSessionVariables(controller: this, httpContext: HttpContext);
+            var viewModel = new EventDetailsViewModel
+            {
+                Activity = activityModel,
+                Place = placeModel
+            };
 
-            return View(activitiesDt);
+            return View(viewModel);
         }
+
     }
 }
 
