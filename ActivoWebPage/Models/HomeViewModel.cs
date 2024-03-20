5e0bfcf7ec3ed77dab1e41ad353a4074aadf4f40
FLinnell <158289006+FLinnell@users.noreply.github.com>
2024-03-20 15:24:10 +0100

    Update HomeViewModel.cs
    lagt till Tags

--- ActivoWebPage/Models/HomeViewModel.cs
+++ ActivoWebPage/Models/HomeViewModel.cs
@@ -5,6 +5,6 @@ namespace ActivoWebPage.Models
         public List<Event?> Events { get; set; } = new List<Event?>();
         public List<Activity?> Activities { get; set; } = new List<Activity?>();
         public Advertisements? RandomAdvertisement { get; set; }
-
+        public List<Tag?> Tags { get; set; } = new List<Tag?>();
     }
 }
